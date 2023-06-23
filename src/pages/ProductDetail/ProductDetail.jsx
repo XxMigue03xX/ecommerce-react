7428ca3d6ef7e23d392603fe812e3156a66573b6
Miguel Garavito <mgaravitocamargo@gmail.com>
2023-06-23 02:26:11 -0500

    styles-product-detail-loader

--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -6,6 +6,7 @@ import { useAddProductToCart } from '../../hooks/queries/useAddProductToCart';
 import { useSelector } from 'react-redux';
 import { useCart } from '../../hooks/queries/useCart';
 import './ProductDetail.css'
+import Loader from '../../components/common/Loader/Loader';
 
 const ProductDetail = () => {
   const navigate = useNavigate();
@@ -46,57 +47,66 @@ const ProductDetail = () => {
     setQuantity(Number(quantityInCart));
   }, [quantityInCart])
 
-  if(isLoading) return <p>Loading product</p>;
-
-  if(isError) return <p>{error.message ?? "No se pudo cargar el producto"}</p>
   
   return (
     <section className='product-detail'>
-      <section className='product-detail__info'>
-        <div className='product-detail__img-section'>
-          <div className='product-detail__img-container'>
-            <img src={data.images[0].url} alt={data.title} />
-          </div>
+      {isLoading && (
+        <div className="loader-product-detail">
+          <Loader/>
         </div>
-        <div className='product-detail__details'>
-          <h3 className='product-detail__brand'>{data.brand}</h3>
-          <h2 className='product-detail__title'>{data.title}</h2>
-          <p className='product-detail__description'>
-            {data.description}
-          </p>
-          <div className='product-detail__price-and-quantity'>
-            <div className='product-detail__price-section'>
-              <h3 className='product-detail__price-label'>Price</h3>
-              <p className='product-detail__price-content'>
-                <en>$ {data.price}</en>
-              </p>
-            </div>
-            <div className='product-detail__quantity-section'>
-              <h3 className='product-detail__quantity-label'>Quantity</h3>
-              <div className='product-detail__quantity-container'>
-                <button className='product-detail__quantity-btn' onClick={decrement}>-</button>
-                <div className='product-detail__quantity-content'>{quantity}</div>
-                <button className='product-detail__quantity-btn' onClick={increment}>+</button>
+      )}
+      {isError && (
+        <p>{error.message ?? "No se pudo cargar el producto"}</p>
+      )}
+      {!isLoading && !isError && (
+          <>
+            <section className='product-detail__info'>
+              <div className='product-detail__img-section'>
+                <div className='product-detail__img-container'>
+                  <img src={data.images[0].url} alt={data.title} />
+                </div>
               </div>
-            </div>
-          </div>
-          {!isProductInCart && (
-            <button className='product-detail__add-btn' onClick={handleAddToCart}>
-              <p>Add to cart</p>
-              <i className='bx bx-cart' ></i>
-            </button>
-          )}
-          {isProductInCart && (
-            <button className='product-detail__add-btn'>
-              <p>Update in cart</p>
-              <i className='bx bx-cart' ></i>
-            </button>
-          )}
-        </div>
-      </section>
-      <h4>Discover similar items</h4>
-      <ProductList categories={data.categoryId} excludedIds={[Number(productId)]}/>
-    </section>
+              <div className='product-detail__details'>
+                <h3 className='product-detail__brand'>{data.brand}</h3>
+                <h2 className='product-detail__title'>{data.title}</h2>
+                <p className='product-detail__description'>
+                  {data.description}
+                </p>
+                <div className='product-detail__price-and-quantity'>
+                  <div className='product-detail__price-section'>
+                    <h3 className='product-detail__price-label'>Price</h3>
+                    <p className='product-detail__price-content'>
+                      <en>$ {data.price}</en>
+                    </p>
+                  </div>
+                  <div className='product-detail__quantity-section'>
+                    <h3 className='product-detail__quantity-label'>Quantity</h3>
+                    <div className='product-detail__quantity-container'>
+                      <button className='product-detail__quantity-btn' onClick={decrement}>-</button>
+                      <div className='product-detail__quantity-content'>{quantity}</div>
+                      <button className='product-detail__quantity-btn' onClick={increment}>+</button>
+                    </div>
+                  </div>
+                </div>
+                {!isProductInCart && (
+                  <button className='product-detail__add-btn' onClick={handleAddToCart}>
+                    <p>Add to cart</p>
+                    <i className='bx bx-cart' ></i>
+                  </button>
+                )}
+                {isProductInCart && (
+                  <button className='product-detail__add-btn'>
+                    <p>Update in cart</p>
+                    <i className='bx bx-cart' ></i>
+                  </button>
+                )}
+              </div>
+            </section>
+            <h4>Discover similar items</h4>
+            <ProductList categories={data.categoryId} excludedIds={[Number(productId)]}/>
+          </>
+        )}
+        </section>
   );
 };
 
